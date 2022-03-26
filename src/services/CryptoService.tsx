35db00660c16cda47f06678bd6410bb403998671
kinga szabo <kinga.johanna.szabo@zooshgroup.com>
2022-03-26 11:49:40 +0100

    react nacigation gesture handler warning fix

--- src/services/CryptoService.tsx
+++ src/services/CryptoService.tsx
@@ -1,44 +1,9 @@
 import axios from "axios";
-import moment from "moment";
-
-const formatSparkline = (numbers: any) => {
-    const sevenDaysAgo = moment().subtract(1, "week").valueOf();
-
-    let formattedSparkline = numbers.map((item: any, index: any) => {
-        return {
-            timestamp: sevenDaysAgo + (index + 1) * 3600,
-            value: item,
-        };
-    });
-
-    return formattedSparkline;
-};
-
-const formatMarketData = (data: any) => {
-    // @ts-ignore
-    let formattedMarketData = [];
-
-    data.message.data.forEach((element: { sparkline_in_7d: { price: number } }) => {
-        const formattedSparkline = formatSparkline(element.sparkline_in_7d.price);
-
-        const formattedElement = {
-            ...element,
-            sparkline_in_7d: {
-                price: formattedSparkline,
-            },
-        };
-
-        formattedMarketData.push(formattedElement);
-    });
-    // @ts-ignore
-    return formattedMarketData;
-};
 
 export const getMarketData = async () => {
     try {
-        const response = await axios.get("http://localhost:1337/crypto/allcoins");
-        const data = response.data;
-        return formatMarketData(data);
+        const response = await axios.get("http://192.168.1.146:1337/crypto/allcoins");
+        return response.data.message;
     } catch (error) {
         console.log(error);
         return null;
